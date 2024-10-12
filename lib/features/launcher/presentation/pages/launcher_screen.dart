0199b38bce7cf95e3804d07f750c5ef66aa3a8d2
Hasan Masrur <mhbabu99@gmail.com>
2024-10-12 23:49:34 +0600

    create launcher_screen

---
+++ lib/features/launcher/presentation/pages/launcher_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+
+class LauncherScreen extends StatefulWidget {
+  const LauncherScreen({super.key});
+
+  @override
+  LauncherScreenState createState() => LauncherScreenState();
+}
+
+class LauncherScreenState extends State<LauncherScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Spacer(),
+
+            Spacer(),
+            // Text(
+            // //  "@Copyright ${AppConstants.appName}",
+            //   style: Theme.of(context).textTheme.bodyMedium,
+            // ),
+            SizedBox(height: 50),
+          ],
+        ),
+      ),
+    );
+  }
+}
