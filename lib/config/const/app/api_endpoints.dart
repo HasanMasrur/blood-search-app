73d640df932c042676493703a629c1ed49e1bb1c
Hasan Masrur <mhbabu99@gmail.com>
2024-10-13 16:45:59 +0600

    create api_endpoints class

---
+++ lib/config/const/app/api_endpoints.dart
@@ -0,0 +1,7 @@
+class ApiEndPoints {
+  ApiEndPoints._privateConstructor();
+  static final ApiEndPoints instance = ApiEndPoints._privateConstructor();
+  static const String baseUrl = "http://54.253.7:8080/";
+  static const String login = "auth/user/login";
+  static const String register = "auth/user/register";
+}
